28dc98236d5afd37f9dd9100b9958b391ab334a5
Max Guppy <theonly@mrcat.au>
2026-08-22 10:45:46 +1000

    Add cut_error throughout parser

--- bi/src/parser.rs
+++ bi/src/parser.rs
@@ -1,6 +1,6 @@
 use winnow::Parser;
 use winnow::combinator::{
-    Postfix, alt, delimited, expression, opt, preceded, repeat, separated, seq, terminated,
+    Postfix, alt, cut_err, delimited, expression, opt, preceded, repeat, separated, seq, terminated,
 };
 use winnow::error::{ContextError, ParseError};
 use winnow::stream::TokenSlice;
@@ -19,7 +19,7 @@ pub fn parse(input: &[Token]) -> Result<Ast, ParseError<TokenSlice<'_, Token>, C
     })
 }
 
-fn decl(input: &mut TokenSlice<Token>) -> winnow::Result<Decl> {
+fn decl(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Decl> {
     alt((
         r#struct.map(Decl::Struct),
         r#enum.map(Decl::Enum),
@@ -28,45 +28,49 @@ fn decl(input: &mut TokenSlice<Token>) -> winnow::Result<Decl> {
     .parse_next(input)
 }
 
-fn r#struct(input: &mut TokenSlice<Token>) -> winnow::Result<Struct> {
-    seq!(
-        _: literal(TokenKind::KStruct),
-        ident,
-        _: literal(TokenKind::OpenBrace),
-        field_list,
-        _: literal(TokenKind::CloseBrace),
+fn r#struct(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Struct> {
+    preceded(
+        literal(TokenKind::KStruct),
+        cut_err(seq!(
+            ident,
+            _: literal(TokenKind::OpenBrace),
+            field_list,
+            _: literal(TokenKind::CloseBrace),
+        )),
     )
     .map(|(ident, fields)| Struct { ident, fields })
     .parse_next(input)
 }
 
-fn field_list(input: &mut TokenSlice<Token>) -> winnow::Result<Vec<Field>> {
+fn field_list(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Vec<Field>> {
     separated(0.., field, literal(TokenKind::Comma)).parse_next(input)
 }
 
-fn field(input: &mut TokenSlice<Token>) -> winnow::Result<Field> {
+fn field(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Field> {
     seq!(ident, _: literal(TokenKind::Colon), r#type)
         .map(|(ident, ty)| Field { ident, ty })
         .parse_next(input)
 }
 
-fn r#enum(input: &mut TokenSlice<Token>) -> winnow::Result<Enum> {
-    seq!(
-        _: literal(TokenKind::KType),
-        ident,
-        _: literal(TokenKind::OpenBrace),
-        case_list,
-        _: literal(TokenKind::CloseBrace),
+fn r#enum(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Enum> {
+    preceded(
+        literal(TokenKind::KType),
+        cut_err(seq!(
+            ident,
+            _: literal(TokenKind::OpenBrace),
+            cut_err(case_list),
+            _: literal(TokenKind::CloseBrace),
+        )),
     )
     .map(|(ident, cases)| Enum { ident, cases })
     .parse_next(input)
 }
 
-fn case_list(input: &mut TokenSlice<Token>) -> winnow::Result<Vec<Case>> {
-    separated(0.., case, literal(TokenKind::Comma)).parse_next(input)
+fn case_list(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Vec<Case>> {
+    separated(0.., cut_err(case), literal(TokenKind::Comma)).parse_next(input)
 }
 
-fn case(input: &mut TokenSlice<Token>) -> winnow::Result<Case> {
+fn case(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Case> {
     alt((
         seq!(
             ident,
@@ -83,15 +87,17 @@ fn case(input: &mut TokenSlice<Token>) -> winnow::Result<Case> {
     .parse_next(input)
 }
 
-fn function(input: &mut TokenSlice<Token>) -> winnow::Result<Function> {
-    seq!(
-        _: literal(TokenKind::KFunction),
-        ident,
-        _: literal(TokenKind::OpenPar),
-        arguments,
-        _: literal(TokenKind::ClosePar),
-        opt(preceded(literal(TokenKind::Arrow), r#type)),
-        block,
+fn function(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Function> {
+    preceded(
+        literal(TokenKind::KFunction),
+        cut_err(seq!(
+            ident,
+            _: literal(TokenKind::OpenPar),
+            arguments,
+            _: literal(TokenKind::ClosePar),
+            opt(preceded(literal(TokenKind::Arrow), r#type)),
+            block,
+        )),
     )
     .map(|(ident, parameters, ret, stmts)| Function {
         signature: FunctionSignature {
@@ -104,28 +110,28 @@ fn function(input: &mut TokenSlice<Token>) -> winnow::Result<Function> {
     .parse_next(input)
 }
 
-fn arguments(input: &mut TokenSlice<Token>) -> winnow::Result<Vec<Parameter>> {
+fn arguments(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Vec<Parameter>> {
     separated(0.., argument, literal(TokenKind::Comma)).parse_next(input)
 }
 
-fn argument(input: &mut TokenSlice<Token>) -> winnow::Result<Parameter> {
+fn argument(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Parameter> {
     seq!(ident, _: literal(TokenKind::Colon), r#type)
         .map(|(label, ty)| Parameter { label, ty })
         .parse_next(input)
 }
 
-fn block(input: &mut TokenSlice<Token>) -> winnow::Result<Vec<Stmt>> {
+fn block(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Vec<Stmt>> {
     delimited(
         literal(TokenKind::OpenBrace),
-        repeat(0.., stmt),
+        cut_err(repeat(0.., stmt)),
         literal(TokenKind::CloseBrace),
     )
     .parse_next(input)
 }
 
-fn stmt(input: &mut TokenSlice<Token>) -> winnow::Result<Stmt> {
+fn stmt(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Stmt> {
     // TODO
-    alt((
+    cut_err(alt((
         r#if.map(Stmt::If),
         r#while.map(Stmt::While),
         var_decl.map(Stmt::VarDecl),
@@ -134,23 +140,26 @@ fn stmt(input: &mut TokenSlice<Token>) -> winnow::Result<Stmt> {
         r#break.map(|_| Stmt::Break),
         r#continue.map(|_| Stmt::Continue),
         r#return.map(Stmt::Return),
-    ))
+    )))
     .parse_next(input)
 }
 
-fn r#if(input: &mut TokenSlice<Token>) -> winnow::Result<IfStmt> {
-    seq!(
-        _: literal(TokenKind::KIf),
-        separated(
-            1..,
-            (delimited(literal(TokenKind::OpenPar), expr, literal(TokenKind::ClosePar)), block)
-                .map(|(condition, stmts)| IfBlock { condition, stmts }),
-            (literal(TokenKind::KElse), literal(TokenKind::KIf)),
-        ),
-        opt(preceded(
-            literal(TokenKind::KElse),
-            block.map(|stmts| ElseBlock { stmts })
-        ))
+fn r#if(input: &mut TokenSlice<Token>) -> winnow::ModalResult<IfStmt> {
+    preceded(
+        literal(TokenKind::KIf),
+        cut_err(seq!(
+            _: literal(TokenKind::KIf),
+            separated(
+                1..,
+                (delimited(literal(TokenKind::OpenPar), expr, literal(TokenKind::ClosePar)), block)
+                    .map(|(condition, stmts)| IfBlock { condition, stmts }),
+                (literal(TokenKind::KElse), literal(TokenKind::KIf)),
+            ),
+            opt(preceded(
+                literal(TokenKind::KElse),
+                block.map(|stmts| ElseBlock { stmts })
+            ))
+        )),
     )
     .map(|(if_blocks, else_block)| IfStmt {
         if_blocks,
@@ -159,31 +168,33 @@ fn r#if(input: &mut TokenSlice<Token>) -> winnow::Result<IfStmt> {
     .parse_next(input)
 }
 
-fn r#while(input: &mut TokenSlice<Token>) -> winnow::Result<WhileStmt> {
+fn r#while(input: &mut TokenSlice<Token>) -> winnow::ModalResult<WhileStmt> {
     preceded(
         literal(TokenKind::KWhile),
-        (
+        cut_err((
             delimited(
                 literal(TokenKind::OpenPar),
                 expr,
                 literal(TokenKind::ClosePar),
             ),
             block,
-        ),
+        )),
     )
     .map(|(condition, stmts)| WhileStmt { condition, stmts })
     .parse_next(input)
 }
 
-fn var_decl(input: &mut TokenSlice<Token>) -> winnow::Result<VarDecl> {
+fn var_decl(input: &mut TokenSlice<Token>) -> winnow::ModalResult<VarDecl> {
     seq!(
         one_of(|t: &Token| *t == TokenKind::KLet || *t == TokenKind::KVar),
-        ident,
-        opt(preceded(literal(TokenKind::Colon), r#type)),
-        opt(preceded(literal(TokenKind::Equals), expr)),
-        _: literal(TokenKind::Semicolon),
+        cut_err(seq!(
+            ident,
+            opt(preceded(literal(TokenKind::Colon), r#type)),
+            opt(preceded(literal(TokenKind::Equals), expr)),
+            _: literal(TokenKind::Semicolon),
+        ))
     )
-    .map(|(mutable, ident, ty, value)| VarDecl {
+    .map(|(mutable, (ident, ty, value))| VarDecl {
         mutable: *mutable == TokenKind::KVar,
         ident,
         ty,
@@ -192,7 +203,7 @@ fn var_decl(input: &mut TokenSlice<Token>) -> winnow::Result<VarDecl> {
     .parse_next(input)
 }
 
-fn assignment(input: &mut TokenSlice<Token>) -> winnow::Result<AssignmentStmt> {
+fn assignment(input: &mut TokenSlice<Token>) -> winnow::ModalResult<AssignmentStmt> {
     seq!(
         place_expr,
         _: literal(TokenKind::Equals),
@@ -202,73 +213,82 @@ fn assignment(input: &mut TokenSlice<Token>) -> winnow::Result<AssignmentStmt> {
     .parse_next(input)
 }
 
-fn expr_stmt(input: &mut TokenSlice<Token>) -> winnow::Result<Expr> {
-    terminated(expr, literal(TokenKind::Semicolon)).parse_next(input)
+fn expr_stmt(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Expr> {
+    terminated(expr, cut_err(literal(TokenKind::Semicolon))).parse_next(input)
 }
 
-fn r#break(input: &mut TokenSlice<Token>) -> winnow::Result<()> {
-    terminated(literal(TokenKind::KBreak), literal(TokenKind::Semicolon))
-        .map(|_| ())
-        .parse_next(input)
+fn r#break(input: &mut TokenSlice<Token>) -> winnow::ModalResult<()> {
+    terminated(
+        literal(TokenKind::KBreak),
+        cut_err(literal(TokenKind::Semicolon)),
+    )
+    .map(|_| ())
+    .parse_next(input)
 }
 
-fn r#continue(input: &mut TokenSlice<Token>) -> winnow::Result<()> {
-    terminated(literal(TokenKind::KContinue), literal(TokenKind::Semicolon))
-        .map(|_| ())
-        .parse_next(input)
+fn r#continue(input: &mut TokenSlice<Token>) -> winnow::ModalResult<()> {
+    terminated(
+        literal(TokenKind::KContinue),
+        cut_err(literal(TokenKind::Semicolon)),
+    )
+    .map(|_| ())
+    .parse_next(input)
 }
 
-fn r#return(input: &mut TokenSlice<Token>) -> winnow::Result<Option<Expr>> {
+fn r#return(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Option<Expr>> {
     delimited(
         literal(TokenKind::KReturn),
-        opt(expr),
-        literal(TokenKind::Semicolon),
+        cut_err(opt(expr)),
+        cut_err(literal(TokenKind::Semicolon)),
     )
     .parse_next(input)
 }
 
-fn expr(input: &mut TokenSlice<Token>) -> winnow::Result<Expr> {
-    expression(alt((
-        e_literal.map(Expr::Literal),
-        function_call.map(Expr::FunctionCall),
-        paren_expr.map(Expr::Paren),
-        struct_init.map(Expr::StructInit),
-        ident.map(Expr::Ident),
-    )))
-    .postfix(preceded(
-        literal(TokenKind::Dot),
-        Postfix(1, |input: &mut _, base| {
-            let member = ident.parse_next(input)?;
-            Ok(Expr::MemberAccess(Box::new(MemberAccessExpr {
-                base,
-                member,
-            })))
-        }),
-    ))
+fn expr(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Expr> {
+    cut_err(
+        expression(alt((
+            e_literal.map(Expr::Literal),
+            function_call.map(Expr::FunctionCall),
+            paren_expr.map(Expr::Paren),
+            struct_init.map(Expr::StructInit),
+            ident.map(Expr::Ident),
+        )))
+        .postfix(preceded(
+            literal(TokenKind::Dot),
+            Postfix(1, |input: &mut _, base| {
+                let member = ident.parse_next(input)?;
+                Ok(Expr::MemberAccess(Box::new(MemberAccessExpr {
+                    base,
+                    member,
+                })))
+            }),
+        )),
+    )
     .parse_next(input)
 }
 
-fn place_expr(input: &mut TokenSlice<Token>) -> winnow::Result<PlaceExpr> {
+fn place_expr(input: &mut TokenSlice<Token>) -> winnow::ModalResult<PlaceExpr> {
     // TODO
-    alt((ident.map(PlaceExpr::Ident),)).parse_next(input)
+    cut_err(alt((ident.map(PlaceExpr::Ident),))).parse_next(input)
 }
 
-fn e_literal(input: &mut TokenSlice<Token>) -> winnow::Result<LiteralExpr> {
+fn e_literal(input: &mut TokenSlice<Token>) -> winnow::ModalResult<LiteralExpr> {
+    // TODO: Number literals
     string_lit.map(LiteralExpr::String).parse_next(input)
 }
 
-fn function_call(input: &mut TokenSlice<Token>) -> winnow::Result<FunctionCallExpr> {
+fn function_call(input: &mut TokenSlice<Token>) -> winnow::ModalResult<FunctionCallExpr> {
     seq!(
         ident,
         _: literal(TokenKind::OpenPar),
-        separated(0.., expr, literal(TokenKind::Comma)),
-        _: literal(TokenKind::ClosePar),
+        cut_err(separated(0.., expr, literal(TokenKind::Comma))),
+        _: cut_err(literal(TokenKind::ClosePar)),
     )
     .map(|(ident, arguments)| FunctionCallExpr { ident, arguments })
     .parse_next(input)
 }
 
-fn paren_expr(input: &mut TokenSlice<Token>) -> winnow::Result<Box<Rich<Expr>>> {
+fn paren_expr(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Box<Rich<Expr>>> {
     seq!(
         literal(TokenKind::OpenPar),
         expr,
@@ -283,7 +303,7 @@ fn paren_expr(input: &mut TokenSlice<Token>) -> winnow::Result<Box<Rich<Expr>>> 
     .parse_next(input)
 }
 
-fn struct_init(input: &mut TokenSlice<Token>) -> winnow::Result<StructInitExpr> {
+fn struct_init(input: &mut TokenSlice<Token>) -> winnow::ModalResult<StructInitExpr> {
     seq!(
         ident,
         _: literal(TokenKind::OpenBrace),
@@ -294,7 +314,7 @@ fn struct_init(input: &mut TokenSlice<Token>) -> winnow::Result<StructInitExpr> 
     .parse_next(input)
 }
 
-fn struct_arg(input: &mut TokenSlice<Token>) -> winnow::Result<StructInitArgument> {
+fn struct_arg(input: &mut TokenSlice<Token>) -> winnow::ModalResult<StructInitArgument> {
     seq!(
         ident,
         _: literal(TokenKind::Colon),
@@ -304,7 +324,7 @@ fn struct_arg(input: &mut TokenSlice<Token>) -> winnow::Result<StructInitArgumen
     .parse_next(input)
 }
 
-fn r#type(input: &mut TokenSlice<Token>) -> winnow::Result<Type> {
+fn r#type(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Type> {
     ident
         .map(|ident| {
             Type::Simple(SimpleType {
@@ -315,7 +335,7 @@ fn r#type(input: &mut TokenSlice<Token>) -> winnow::Result<Type> {
         .parse_next(input)
 }
 
-fn ident(input: &mut TokenSlice<Token>) -> winnow::Result<Rich<String>> {
+fn ident(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Rich<String>> {
     literal(TokenKind::Id)
         .map(|name: &[Token]| Rich {
             value: name[0].value.clone(),
@@ -324,7 +344,7 @@ fn ident(input: &mut TokenSlice<Token>) -> winnow::Result<Rich<String>> {
         .parse_next(input)
 }
 
-fn string_lit(input: &mut TokenSlice<Token>) -> winnow::Result<Rich<String>> {
+fn string_lit(input: &mut TokenSlice<Token>) -> winnow::ModalResult<Rich<String>> {
     literal(TokenKind::String)
         .map(|name: &[Token]| Rich {
             value: name[0].value.clone(),
